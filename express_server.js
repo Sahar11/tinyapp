1885a7ba5ff3c127e42f14a8f03aed3aa8b1c4ff
Sahar <saharhalim151@gmail.com>
2021-12-07 00:56:13 +0000

    changes

--- express_server.js
+++ express_server.js
@@ -16,6 +16,10 @@ app.get("/", (req, res) => {
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
 });
+app.get("/urls", (req, res) => {
+  const templateVars = { urls: urlDatabase };
+  res.render("urls_index", templateVars);
+});
 app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
