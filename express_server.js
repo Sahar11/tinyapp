fb09df23499984d40daa20eb1075b8481965a702
Sahar <saharhalim151@gmail.com>
2021-12-07 01:46:00 +0000

    adding URLs

--- express_server.js
+++ express_server.js
@@ -20,6 +20,12 @@ app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
 });
+
+app.get("/urls/:shortURL", (req, res) => {
+  const templateVars = { shortURL: req.params.shortURL, longURL: req.params.longURL };
+  res.render("urls_show", templateVars);
+});
+
 app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
